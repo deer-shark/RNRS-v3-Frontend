d63302d3fe1d7a05b886b6731f78cf03ac7f6990
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-12 20:00:22 +0800

    lint(eslint): the props should bound with propTypes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Container } from "react-bootstrap";
 
 export default function Title(props) {
+  const { content } = props;
+
   return (
     <Container className="title-container">
       <div className="d-flex justify-content-center align-self-center">
-        <h1 className="title-container-title">{props.content}</h1>
+        <h1 className="title-container-title">{content}</h1>
       </div>
     </Container>
   );
 }
+
+Title.propTypes = {
+  content: PropTypes.string.isRequired,
+};
 
 /*
     <div className={'header-container-text d-flex justify-content-center align-self-center'}>
