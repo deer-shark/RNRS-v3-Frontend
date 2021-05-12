d697e7b74375945d8a2af4477dc4f4bf0f61b519
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-12 19:44:35 +0800

    lint(eslint): js -> jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
-import routesConfig from "./config/routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -9,6 +8,7 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import routesConfig from "./config/routes";
 // import './css/style.css';
 import "./css/App.css";
 import "./css/Main.css";
