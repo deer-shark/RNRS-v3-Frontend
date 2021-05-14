dd04b8f6caf933ce44f829a9f992a9f3970fd1a5
yc97463 <youualan87@gmail.com>
2021-05-15 00:25:26 +0800

    apple

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, ListGroup, ListGroupItem, Badge } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../css/list-group.css";
 
 export default function EventList() {
   return (
     <>
       <Container className="info-container">
-        <h2>EventList</h2>
+        <h2>所有進行中活動</h2>
+        <ListGroup className="toc-container" id="toc">
+          <ListGroupItem action className="rnrs-list-item">
+            <h5>Stray 迷途</h5>
+            <Badge variant="warning">
+              <FontAwesomeIcon icon={["far", "clock"]} /> 2021/05/07
+            </Badge>{" "}
+            <Badge variant="secondary">
+              <FontAwesomeIcon icon={["far", "map"]} /> 高師大附中
+            </Badge>
+          </ListGroupItem>
+        </ListGroup>
       </Container>
     </>
   );
