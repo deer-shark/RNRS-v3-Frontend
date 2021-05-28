f6bb66167e2e2d7254d8024828519e235f8b3b05
Sz-Wei Chen <swchen1217@gmail.com>
2021-05-28 23:13:25 +0800

    Axios interceptors,validateStatus

--- src/API.js
+++ src/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import Swal from "sweetalert2";
 
-const productionBaseURL = "";
-const developmentBaseURL = "";
+const productionBaseURL = "https://api.rnrs.deershark.com";
+const developmentBaseURL = "http://localhost:3030";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? productionBaseURL
@@ -10,15 +11,42 @@ const baseURL =
 const instance = axios.create({
   baseURL,
   validateStatus(status) {
-    return status >= 200 && status < 600;
+    return status < 500;
   },
 });
 
-instance.interceptors.request.use(function (config) {
-  return config;
-});
-instance.interceptors.response.use(function (response) {
-  return response;
-});
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    Swal.fire({
+      title: "伺服器錯誤!",
+      text: "請洽詢 主辦單位 或 Deershark 。",
+      icon: "error",
+    });
+    Promise.reject(err);
+  }
+);
+instance.interceptors.response.use(
+  (res) => {
+    if (res.status === 403) {
+      Swal.fire({
+        title: "權限錯誤!",
+        text: "請先登入，或你沒有權限操作此功能。",
+        icon: "error",
+      });
+    }
+    return res;
+  },
+  (err) => {
+    Swal.fire({
+      title: "伺服器錯誤!",
+      text: "請洽詢 主辦單位 或 Deershark 。",
+      icon: "error",
+    });
+    Promise.reject(err);
+  }
+);
 
 export default instance;
