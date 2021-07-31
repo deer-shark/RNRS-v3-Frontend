5554c2dc8fe1a1239ac4ce4a6d197a5e8c77adff
Sz-Wei Chen <swchen1217@gmail.com>
2021-07-31 14:45:02 +0800

    Upload 202107311445

--- src/components/DeclareForm.jsx
+++ src/components/DeclareForm.jsx
@@ -1,25 +1,76 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import QRCode from "qrcode.react";
 import Title from "./Title";
 import StoreContext from "../store/StoreContext";
 import { setBackground } from "../store/actions/appAction";
+import API from "../API";
+
+const ReactSwal = withReactContent(Swal);
 
 export default function DeclareForm(props) {
   const { event } = props;
   const { state, dispatch } = useContext(StoreContext);
   const { backgroundName } = state.app;
+  const [orgId, setOrgId] = useState("");
+  const [roleId, setRoleId] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     dispatch(setBackground(event.backgroundImage));
   }, [backgroundName]);
 
+  function validateForm() {
+    return (
+      orgId !== "" &&
+      roleId !== "" &&
+      name.length > 0 &&
+      phone.length > 0 &&
+      email.length > 0
+    );
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const payload = { orgId, roleId, name, phone, email };
+    await API.post(`/declare/${event.code}`, payload)
+      .then((res) => {
+        console.log(res);
+        switch (res.status) {
+          case 201: {
+            const { hash, googleForm } = res.data;
+            if (!googleForm) {
+              ReactSwal.fire({
+                title: "填報完成！",
+                html: <QRCode value={hash} size="255" level="H" />,
+                footer:
+                  "<ol><li>請妥善保存您的 QR Code 以供入場查驗用。</li></ol>",
+              });
+            } else {
+              console.log("GoogleForm");
+            }
+            break;
+          }
+          default:
+            break;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
       <Title content={event.name} />
       <Container className="info-container">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <h2>身分資料填報</h2>
           <Row className="question-part">
             <Col className="col-12">
@@ -67,13 +118,16 @@ export default function DeclareForm(props) {
                   as="select"
                   id="form-register-org"
                   defaultValue=""
+                  onChange={(e) => {
+                    setOrgId(e.target.value);
+                  }}
                 >
                   <option value="" disabled>
                     請選擇單位
                   </option>
                   {/* eslint-disable-next-line react/prop-types */}
                   {event.orgs.map((item) => (
-                    <option value={item.value} key={item.id}>
+                    <option value={item.id} key={item.id}>
                       {item.value}
                     </option>
                   ))}
@@ -89,13 +143,16 @@ export default function DeclareForm(props) {
                   as="select"
                   id="form-register-role"
                   defaultValue=""
+                  onChange={(e) => {
+                    setRoleId(e.target.value);
+                  }}
                 >
                   <option value="" disabled>
                     請選擇身分
                   </option>
                   {/* eslint-disable-next-line react/prop-types */}
                   {event.roles.map((item) => (
-                    <option value={item.value} key={item.id}>
+                    <option value={item.id} key={item.id}>
                       {item.value}
                     </option>
                   ))}
@@ -116,6 +173,9 @@ export default function DeclareForm(props) {
                   type="text"
                   id="form-register-name"
                   placeholder="姓名"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                 />
               </Form.Group>
             </Col>
@@ -134,6 +194,9 @@ export default function DeclareForm(props) {
                   type="text"
                   id="form-register-phone"
                   placeholder="行動電話"
+                  onChange={(e) => {
+                    setPhone(e.target.value);
+                  }}
                 />
               </Form.Group>
             </Col>
@@ -146,6 +209,9 @@ export default function DeclareForm(props) {
                   type="text"
                   id="form-register-email"
                   placeholder="電子郵件"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
                 />
               </Form.Group>
             </Col>
@@ -156,9 +222,10 @@ export default function DeclareForm(props) {
                 className="btn-rnrs"
                 block
                 size="lg"
-                href="#info-cdc-healthy"
+                disabled={!validateForm()}
+                type="submit"
               >
-                前往下一步：健康聲明書
+                {event.googleFormSrc ? "前往下一步：健康聲明書" : "送出填報"}
               </Button>
             </Col>
           </Row>
@@ -190,7 +257,7 @@ DeclareForm.propTypes = {
     name: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
-    googleFormSrc: PropTypes.string.isRequired,
+    googleFormSrc: PropTypes.string,
     backgroundImage: PropTypes.string.isRequired,
     orgs: PropTypes.arrayOf(
       PropTypes.shape({
